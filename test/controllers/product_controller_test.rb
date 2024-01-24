557fd6c5e7a4d7d449049aafb959f50dc6df4aeb
Raúl Pedro <raulpedromega@gmail.com>
2024-01-24 16:57:11 -0600

    Pruebas a la App

--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -3,9 +3,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test 'render a list of products' do
     get products_path
     assert_response :success
-    assett_select '.product', 2
+    assert_select '.product', 2
   end
   test 'render a details product page' do
-
+    get product_path(products(:iphone14).id) # el .id es opcional
+    assert_response :success
+    assert_select '.title', 'iPhone 14'
+    assert_select '.description', 'En buen estado'
+    assert_select '.price', '4000'
   end
 end
