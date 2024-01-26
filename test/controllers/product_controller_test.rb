788c6dd3ef37fb015cdb0220f66e92073f575478
Raúl Pedro <raulpedromega@gmail.com>
2024-01-26 12:21:53 -0600

    Validación Not Null a BD, Prueba unitaria para la validación

--- test/controllers/product_controller_test.rb
+++ test/controllers/product_controller_test.rb
@@ -27,4 +27,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     }
     assert_redirected_to products_path
   end
+  test 'does not allow to create a new product with empty fields' do
+    post products_path, params: {
+      product: {
+        title: '',
+        description: 'La pila no funciona',
+        price: 6000
+      }
+    }
+    assert_response :unprocessable_entity
+  end
 end
