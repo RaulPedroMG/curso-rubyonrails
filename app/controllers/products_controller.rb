575ea4a9981d84a37055f48f8600f1e2d1ce05a0
Raúl Pedro <raulpedromega@gmail.com>
2024-01-30 15:48:52 -0600

    Eliminando productos con destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,11 @@ class ProductsController < ApplicationController
       render :edit, status: :unprocessable_entity
     end
   end
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente.', status: :see_other
+  end
   private
   def product_params
     params.require(:product).permit(:title, :description, :price)
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   post '/products', to: 'products#create'
   get '/products/:id/edit', to: 'products#edit', as: :edit_product
   patch '/products/:id', to: 'products#update'
+  delete '/products/:id', to: 'products#destroy'
   # Defines the root path route ("/")
   # root "posts#index"
 end
